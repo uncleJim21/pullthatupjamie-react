3ba3cbb20721b6289a44355ddad4fcc097aae2f3
uncleJim21 <jim.carucci@protonmail.com>
2024-12-22 19:17:42 -0600

    unfuck styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,38 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
-  content: [],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+      boxShadow: {
+        'white-glow': '0 0 10px rgba(255, 255, 255, 0.8)',
+      },
+      typography: (theme) => ({
+        invert: {
+          css: {
+            '--tw-prose-body': theme('colors.gray.300'),
+            '--tw-prose-headings': theme('colors.white'),
+            '--tw-prose-links': theme('colors.blue.400'),
+            '--tw-prose-bold': theme('colors.white'),
+            '--tw-prose-bullets': theme('colors.gray.600'),
+            '--tw-prose-quotes': theme('colors.gray.100'),
+            '--tw-prose-code': theme('colors.white'),
+            '--tw-prose-pre-bg': theme('colors.gray.900'),
+            '--tw-prose-th-borders': theme('colors.gray.600'),
+            '--tw-prose-td-borders': theme('colors.gray.700'),
+          },
+        },
+      }),
+    },
   },
-  plugins: [],
-}
-
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
+};
