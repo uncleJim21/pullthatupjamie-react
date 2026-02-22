2de78f2b2d100d1990065e6bfbf07d51d76127e6
Jones <jones@Thomass-Mac-mini.local>
2026-02-22 17:35:04 -0600

    feat: add /app/poast route for X-POAST page

--- src/index.js
+++ src/index.js
@@ -18,6 +18,7 @@ import BrowserTestInput from './components/BrowserTestInput.tsx';
 import BlogIndex from './components/blog/BlogIndex.tsx';
 import BlogPost from './components/blog/BlogPost.tsx';
 import UpgradePage from './components/UpgradePage.tsx';
+import PoastPage from './components/PoastPage.tsx';
 import { DEBUG_MODE } from './constants/constants.ts';
 import { inject } from "@vercel/analytics"
 
@@ -140,6 +141,7 @@ const App = () => (
       <Route path="/app/feed/:feedId/jamieProHistory" element={<PodcastFeedPage initialView="jamiePro" defaultTab="history" />} />
       <Route path="/app/feed/:feedId/myRssVideos" element={<PodcastFeedPage initialView="uploads" defaultTab="rss-feed" />} />
       <Route path="/app/automation-settings" element={<AutomationSettingsPage />} />
+      <Route path="/app/poast" element={<PoastPage />} />
       <Route path="/try-jamie" element={<TryJamieWizard />} />
       <Route path="/auth/twitter/complete" element={<TwitterAuthCallback />} />
       {DEBUG_MODE && <Route path="/twitter-test" element={<TwitterTest />} />}
